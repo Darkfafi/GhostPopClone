b4b347f25943db4e844f88d0cd6b8530f9588c56
Darkfafi <ik_ben_ramses@hotmail.com>
2018-07-22 18:46:42 +0200

    Working on level generation

---
+++ Assets/_Core/Scripts/EndlessGame/LevelGeneration/Chunk.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public struct Chunk
+{
+    public GameObject ChunkObject { get; private set; }
+    public int ChunkIndex;
+    public ChunkTheme ChunkTheme { get; private set; }
+
+    public Chunk(int chunkTotalIndex, GameObject chunkObject, ChunkTheme chunkTheme)
+    {
+        ChunkIndex = chunkTotalIndex;
+        ChunkObject = chunkObject;
+        ChunkTheme = chunkTheme;
+    }
+}
+
+[Serializable]
+public struct ChunkTheme
+{
+    public Color ThemeColor
+    {
+        get
+        {
+            return _themeColor;
+        }
+    }
+
+    [SerializeField]
+    private Color _themeColor;
+
+    public ChunkTheme(Color themeColor)
+    {
+        _themeColor = themeColor;
+    }
+}
